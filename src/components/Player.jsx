a77be2d3dee0a51abf25fed80391f9f93e3e8ab0
Reynard Gerber <reynardgerber22@gmail.com>
2026-06-08 18:47:08 +0200

    75. Concept Repetition: Working with State

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react'
+
 export const Player = ({name, symbol}) => {
+
+    const [isEditing, setIsEditing] = useState(false)
+    
     return (
         <li>
           <span className="player">
-            <span className="player-name">{name}</span>
+            {isEditing ? <input required type="text" value={name} /> : <span className="player-name">{name}</span>}
             <span className="player-symbol">{symbol}</span>
           </span>
-          <button>Edit</button>
+          <button onClick={() => setIsEditing(!isEditing)}>Edit</button>
         </li>
     )
 }
