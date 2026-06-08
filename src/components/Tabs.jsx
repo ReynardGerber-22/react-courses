9ee2c0c6a696500f7fbb42e1a472843e79f57c4d
Reynard Gerber <reynardgerber22@gmail.com>
2026-06-08 18:18:34 +0200

    70. Onwards To The Next Project & Advanced Concepts

--- src/components/Tabs.jsx
+++
@@ -1,9 +0,0 @@
-export const Tabs = ({children, buttons, ButtonsContainer = "menu"}) => {
-    return ( <>
-        <ButtonsContainer>
-        {buttons}
-        </ButtonsContainer>
-        {children}
-    </>
-    )
-}
