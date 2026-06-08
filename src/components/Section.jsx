d0dce5511f9c27be8a3c2e8538d6b4ffe37c355e
Reynard Gerber <reynardgerber22@gmail.com>
2026-06-08 08:56:01 +0200

    66: Forwarding Props To Wrapped Elements

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,6 @@
-export const Section = ({ title, children }) => {
+export const Section = ({ title, children, ...props }) => {
     return (
-        <section>
+        <section {...props}>
             <h2>{title}</h2>
             {children}
         </section>
