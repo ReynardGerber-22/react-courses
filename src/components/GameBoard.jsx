2156e10e66089cf2a3254b09a9ea2fdccd459953
Reynard Gerber <reynardgerber22@gmail.com>
2026-06-10 11:46:10 +0200

    81. Best Practice: Updating Ibject State Immutably

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import {useState} from 'react'
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,16 +7,27 @@ const initialGameBoard = [
 ]
 
 export const GameBoard = () => {
+
+    const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = 'X'
+            return updatedBoard;
+        });
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, index) => {
+            {gameBoard.map((row, index) => {
                 return (
                 <li key={index}>
                     <ol>
                         {row.map((column, colIndex) => {
                             return (
                             <li key={colIndex}>
-                                <button>{column}</button>
+                                <button onClick={() => {handleSelectSquare(index,colIndex)}}>{column}</button>
                             </li>
                             )
                         }
