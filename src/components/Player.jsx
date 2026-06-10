35a22222f2eb3c1ea7ec6e0d0a982d3c9b2c8620
Reynard Gerber <reynardgerber22@gmail.com>
2026-06-10 11:19:42 +0200

    79. User Input & Two Way Binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
-export const Player = ({name, symbol}) => {
+export const Player = ({initialName, symbol}) => {
 
     const [isEditing, setIsEditing] = useState(false)
-    
+    const [name, setName] = useState(initialName)
     return (
         <li>
           <span className="player">
-            {isEditing ? <input required type="text" value={name} /> : <span className="player-name">{name}</span>}
+            {isEditing ? <input required type="text" value={name} onChange={(e) => setName(e.target.value)} /> : <span className="player-name">{name}</span>}
             <span className="player-symbol">{symbol}</span>
           </span>
           <button onClick={() => setIsEditing((editing) => !editing)}>{isEditing ? 'Save' : 'Edit'}</button>
