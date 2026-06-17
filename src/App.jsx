64f477db0c491eb9ae3bc9789b2b81d00b24b0df
Reynard Gerber <reynardgerber22@gmail.com>
2026-06-17 09:47:03 +0200

    90. Lifting Computed Values Up

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,29 @@ function deriveActivePlayer(gameTurns) {
 function App() {
 
   const [gameTurns, setGameTurns] = useState([])
-
   const activePlayer = deriveActivePlayer(gameTurns)
+  const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ]
+
+  let gameBoard = initialGameBoard;
+
+  console.log(turns);
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[].player
+    const secondSquareSymbol = gameTurns.player
+    const thirdSquareSymbol = gameTurns.player
+
+  }
 
   function handleSelectSquare(row, col) {
     setGameTurns(prevTurns => {
@@ -36,7 +57,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard turns={gameTurns} activePlayerSymbol={activePlayer} onSelectSquare={handleSelectSquare} />
+        <GameBoard board={gameBoard} onSelectSquare={handleSelectSquare} />
       </div>
       <Log gameTurns={gameTurns} />
     </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,37 +1,22 @@
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-]
 
-export const GameBoard = ({ onSelectSquare, turns }) => {
 
-    let gameBoard = initialGameBoard;
-
-    console.log(turns);
-
-    for (const turn of turns) {
-        const {square, player} = turn;
-        const {row, col} = square;
-        gameBoard[row][col] = player;
-    }
-
+export const GameBoard = ({ onSelectSquare, board }) => {
     return (
         <ol id="game-board">
-            {gameBoard.map((row, index) => {
+            {board.map((row, index) => {
                 return (
-                <li key={index}>
-                    <ol>
-                        {row.map((column, colIndex) => {
-                            return (
-                            <li key={colIndex}>
-                                <button onClick={() => onSelectSquare(index,colIndex)} disabled={column != null}>{column}</button>
-                            </li>
-                            )
-                        }
-                        )}
-                    </ol>
-                </li>
+                    <li key={index}>
+                        <ol>
+                            {row.map((column, colIndex) => {
+                                return (
+                                    <li key={colIndex}>
+                                        <button onClick={() => onSelectSquare(index, colIndex)} disabled={column != null}>{column}</button>
+                                    </li>
+                                )
+                            }
+                            )}
+                        </ol>
+                    </li>
                 )
             })}
         </ol>
