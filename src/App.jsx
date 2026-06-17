7992215acc14e8e3c8461d8b982f5ac5ba4d9ef4
Reynard Gerber <reynardgerber22@gmail.com>
2026-06-17 09:40:27 +0200

    Outsourcing Data into seperate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Player } from './components/Player'
 import { GameBoard } from './components/GameBoard'
-import {useState} from 'react'
+import { useState } from 'react'
 import { Log } from "./components/Log"
-
+import { WINNING_COMBINATIONS } from 'winning-combinations.js'
 
 function deriveActivePlayer(gameTurns) {
-let currentPlayer = 'X';
+  let currentPlayer = 'X';
 
-  if (gameTurns.length > 0  && gameTurns[0].player === "X") {
-      currentPlayer = "O"
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O"
   }
   return currentPlayer;
 }
@@ -23,23 +23,23 @@ function App() {
     setGameTurns(prevTurns => {
       const currentPlayer = deriveActivePlayer(prevTurns);
       const updatedTurns = [
-        {square: {row, col}, player: currentPlayer},...prevTurns
+        { square: { row, col }, player: currentPlayer }, ...prevTurns
       ]
       return updatedTurns;
     })
   }
 
   return (
-   <main>
-    <div id="game-container">
-      <ol id="players" className="highlight-player">
-        <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
-        <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
-      </ol>
-      <GameBoard turns={gameTurns} activePlayerSymbol={activePlayer} onSelectSquare={handleSelectSquare} />
-    </div>
-    <Log gameTurns={gameTurns} />
-   </main>
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
+        </ol>
+        <GameBoard turns={gameTurns} activePlayerSymbol={activePlayer} onSelectSquare={handleSelectSquare} />
+      </div>
+      <Log gameTurns={gameTurns} />
+    </main>
   )
 }
 
