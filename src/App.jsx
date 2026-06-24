69ba1d3cbe187fc0d92bfd2b38aadd6b2cf55eb3
Reynard Gerber <reynardgerber22@gmail.com>
2026-06-24 10:46:50 +0200

    When NOT to lift state up

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
 
+  const [players, setPlayers] = useState({ 'X': 'Player 1', 'O': 'Player 2' })
   const [gameTurns, setGameTurns] = useState([])
   const activePlayer = deriveActivePlayer(gameTurns)
   const initialGameBoard = [
@@ -60,12 +61,21 @@ function App() {
     setGameTurns([])
   }
 
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      }
+    })
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
+          <Player onPlayerNameChange={handlePlayerNameChange} initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player onPlayerNameChange={handlePlayerNameChange} initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
         {(winner || hasDraw) && <GameOver onRestart={handleRestart} winner={winner}></GameOver>}
         <GameBoard board={gameBoard} onSelectSquare={handleSelectSquare} />
