b831aafa120ad84a2a66c82f52633b72e979fa49
Reynard Gerber <reynardgerber22@gmail.com>
2026-06-24 10:40:00 +0200

    Why Immutability Matters Always

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     [null, null, null],
   ]
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -56,6 +56,10 @@ function App() {
     })
   }
 
+  function handleRestart() {
+    setGameTurns([])
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -63,7 +67,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner}></GameOver>}
+        {(winner || hasDraw) && <GameOver onRestart={handleRestart} winner={winner}></GameOver>}
         <GameBoard board={gameBoard} onSelectSquare={handleSelectSquare} />
       </div>
       <Log gameTurns={gameTurns} />
